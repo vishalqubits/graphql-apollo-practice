97adf4deeb7797472b702cb6791d301cc9a66463
“vishalqubits” <vishalqubits@gmail.com>
2023-11-07 16:06:14 +0545

    setup apollo client for frontend

--- backend/src/resolvers/mutation.ts
+++ backend/src/resolvers/mutation.ts
@@ -3,8 +3,16 @@ import { MutationResolvers } from "@app/__generated__/resolvers-types";
 // Use the generated `MutationResolvers` type to type check our mutations!
 const Mutation: MutationResolvers = {
   addBook: async (_, { input }, { prisma }) => {
-    console.log(prisma);
-    return null;
+    await prisma.book.create({
+      data: {
+        title: input.title,
+        author: input.author,
+      },
+    });
+    return {
+      success: true,
+      message: "Successfully book added",
+    };
   },
 };
 
